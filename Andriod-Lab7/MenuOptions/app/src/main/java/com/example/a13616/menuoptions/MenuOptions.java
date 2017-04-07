066a40489305aff8d3de897a55c7150a9da35c38
xuelang-zzz <1361601030@qq.com>
2017-04-07 16:42:49 +0800

    20170407

---
+++ Andriod-Lab7/MenuOptions/app/src/main/java/com/example/a13616/menuoptions/MenuOptions.java
@@ -0,0 +1,32 @@
+package com.example.a13616.menuoptions;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MenuOptions extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stubme
+        menu.add(0,1,2, "保存");
+        menu.add(0,2,1,R.string.exit);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        if(item.getItemId()==2){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ Andriod-Lab7/MenuSUB/app/src/main/java/com/example/a13616/menusub/MenuSUB.java
@@ -0,0 +1,26 @@
+package com.example.a13616.menusub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.SubMenu;
+
+public class MenuSUB extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        SubMenu sub=menu.addSubMenu(R.string.title);
+        sub.setIcon(android.R.drawable.ic_menu_search);
+        sub.add(0,1,2,R.string.local);
+        sub.add(0,2,1,R.string.network);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+}
